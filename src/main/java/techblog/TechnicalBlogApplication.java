330334398dc308c5547be2a5f4a2cf2902eca45b
sourav.ind2 <sourav.ind2@gmail.com>
2020-05-24 19:44:08 +0530

    minor changes

--- src/main/java/techblog/TechnicalBlogApplication.java
+++ src/main/java/techblog/TechnicalBlogApplication.java
@@ -8,8 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class TechnicalBlogApplication {
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
         SpringApplication.run(TechnicalBlogApplication.class, args);
     }
 }
